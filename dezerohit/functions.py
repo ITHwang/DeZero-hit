49eae4e98c84d70dd59ced2eb474309b9d6c62b6
hwang <intaek428@gmail.com>
2021-05-31 21:11:48 +0900

    up to step 47

--- dezerohit/functions.py
+++ dezerohit/functions.py
@@ -7,214 +7,244 @@ from dezerohit.core import Function, Variable, as_variable, as_array
 # Basic functions: sin / cos / tanh / exp / log
 # ========================================================
 
+
 class Sin(Function):
     def forward(self, x):
         y = np.sin(x)
         return y
 
     def backward(self, gy):
-        x, = self.inputs
+        (x,) = self.inputs
         gx = gy * cos(x)
         return gx
 
+
 def sin(x):
     return Sin()(x)
+
 
 class Cos(Function):
     def forward(self, x):
         y = np.cos(x)
         return y
-    
+
     def backward(self, gy):
-        x, = self.inputs
+        (x,) = self.inputs
         gx = gy * -sin(x)
         return gx
 
+
 def cos(x):
     return Cos()(x)
+
 
 class Tanh(Function):
     def forward(self, x):
         y = np.tanh(x)
         return y
-    
+
     def backward(self, gy):
         y = self.outputs[0]()
         gx = gy * (1 - y * y)
         return gx
 
+
 def tanh(x):
     return Tanh()(x)
+
 
 class Exp(Function):
     def forward(self, x):
         y = np.exp(x)
         return y
-    
+
     def backward(self, gy):
-        y = self.outputs[0]() # weakref
+        y = self.outputs[0]()  # weakref
         gx = gy * y
         return gx
 
+
 def exp(x):
     return Exp()(x)
+
 
 class Log(Function):
     def forward(self, x):
         y = np.log(x)
         return y
-    
+
     def backward(self, gy):
-        x, = self.inputs
+        (x,) = self.inputs
         gx = gy / x
         return gx
 
+
 def log(x):
     return Log()(x)
+
 
 # ========================================================
 # Tensor operations: reshape / transpose / get_item
 # ========================================================
 
+
 class Reshape(Function):
     def __init__(self, shape):
         self.shape = shape
-    
+
     def forward(self, x):
         self.x_shape = x.shape
-        y = x.reshape(self.shape) # Note: x is ndarray instance.
+        y = x.reshape(self.shape)  # Note: x is ndarray instance.
         return y
-    
+
     def backward(self, gy):
-        return reshape(gy, self.x_shape) # Note: gy is Variable instance.
+        return reshape(gy, self.x_shape)  # Note: gy is Variable instance.
+
 
 def reshape(x, shape):
     if x.shape == shape:
         return as_variable(x)
     return Reshape(shape)(x)
 
+
 class Transpose(Function):
     def __init__(self, axes=None):
         self.axes = axes
-    
+
     def forward(self, x):
         y = x.transpose(self.axes)
         return y
-    
+
     def backward(self, gy):
         if self.axes is None:
             return transpose(gy)
-        
+
         axes_len = len(self.axes)
-        inv_axes = tuple(np.argsort([ax % axes_len for ax in self.axes])) # same as self.axes, ax % axes_len: check indices.
+        inv_axes = tuple(
+            np.argsort([ax % axes_len for ax in self.axes])
+        )  # same as self.axes, ax % axes_len: check indices.
         return transpose(gy, inv_axes)
+
 
 def transpose(x, axes=None):
     return Transpose(axes)(x)
 
+
 class GetItem(Function):
     def __init__(self, slices):
         self.slices = slices
-    
+
     def forward(self, x):
         y = x[self.slices]
         return y
-    
+
     def backward(self, gy):
-        x, = self.inputs
+        (x,) = self.inputs
         f = GetItemGrad(self.slices, x.shape)
         return f(gy)
+
 
 class GetItemGrad(Function):
     def __init__(self, slices, in_shape):
         self.slices = slices
         self.in_shape = in_shape
-    
+
     def forward(self, gy):
         gx = np.zeros(self.in_shape)
         np.add.at(gx, self.slices, gy)
         return gx
-    
+
     def backward(self, ggx):
         return get_item(ggx, self.slices)
+
 
 def get_item(x, slices):
     f = GetItem(slices)
     return f(x)
 
 
-
 # ========================================================
 # sum / sum_to / broadcast_to / matmul / linear
 # ========================================================
+
 
 class Sum(Function):
     def __init__(self, axis, keepdims):
         self.axis = axis
         self.keepdims = keepdims
-    
+
     def forward(self, x):
         self.x_shape = x.shape
         y = x.sum(axis=self.axis, keepdims=self.keepdims)
         return y
-    
+
     def backward(self, gy):
         gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
         gx = broadcast_to(gy, self.x_shape)
         return gx
 
+
 def sum(x, axis=None, keepdims=False):
     return Sum(axis, keepdims)(x)
+
 
 class SumTo(Function):
     def __init__(self, shape):
         self.shape = shape
-    
+
     def forward(self, x):
         self.x_shape = x.shape
-        y = utils.sum_to(x, self.shape) # Don't be confused utils.sum_to and functions.sum_to
+        y = utils.sum_to(
+            x, self.shape
+        )  # Don't be confused utils.sum_to and functions.sum_to
         return y
-    
+
     def backward(self, gy):
         gx = broadcast_to(gy, self.x_shape)
         return gx
+
 
 def sum_to(x, shape):
     if x.shape == shape:
         return as_variable(x)
     return SumTo(shape)(x)
 
+
 class BroadcastTo(Function):
     def __init__(self, shape):
         self.shape = shape
-    
+
     def forward(self, x):
         self.x_shape = x.shape
         y = np.broadcast_to(x, self.shape)
         return y
-    
+
     def backward(self, gy):
         gx = sum_to(gy, self.x_shape)
         return gx
+
 
 def broadcast_to(x, shape):
     if x.shape == shape:
         return as_variable(x)
     return BroadcastTo(shape)(x)
 
+
 class MatMul(Function):
     def forward(self, x, W):
         y = x.dot(W)
         return y
-    
+
     def backward(self, gy):
         x, W = self.inputs
         gx = matmul(gy, W.T)
         gW = matmul(x.T, gy)
         return gx, gW
 
+
 def matmul(x, W):
     return MatMul()(x, W)
+
 
 class Linear(Function):
     def forward(self, x, W, b):
@@ -222,7 +252,7 @@ class Linear(Function):
         if b is not None:
             y += b
         return y
-    
+
     def backward(self, gy):
         x, W, b = self.inputs
         gb = None if b.data is None else sum_to(gy, b.shape)
@@ -230,25 +260,30 @@ class Linear(Function):
         gW = matmul(x.T, gy)
         return gx, gW, gb
 
+
 def linear(x, W, b=None):
     return Linear()(x, W, b)
+
 
 # ========================================================
 # activation function: sigmoid / softmax
 # ========================================================
 
+
 class Sigmoid(Function):
     def forward(self, x):
-        y = np.tanh(x * 0.5) * 0.5 + 0.5 # tanh(x) = 2sigmoid(2x) - 1
+        y = np.tanh(x * 0.5) * 0.5 + 0.5  # tanh(x) = 2sigmoid(2x) - 1
         return y
-    
+
     def backward(self, gy):
-        y = self.outputs[0]() # weak ref
+        y = self.outputs[0]()  # weak ref
         gx = gy * y * (1 - y)
         return gx
 
+
 def sigmoid(x):
     return Sigmoid()(x)
+
 
 def softmax_simple(x, axis=1):
     x = as_variable(x)
@@ -258,25 +293,60 @@ def softmax_simple(x, axis=1):
 
 
 # ========================================================
-# loss function: mean_squared_error
+# loss function: mean_squared_error / softmax_cross_entropy
 # ========================================================
 
-class MeanSquaredError(Function): # for simple computational graph (memory optimization)
+# for simple computational graph (memory optimization)
+class MeanSquaredError(Function):
     def forward(self, x0, x1):
         diff = x0 - x1
         y = (diff ** 2).sum() / len(diff)
         return y
-    
+
     def backward(self, gy):
         x0, x1 = self.inputs
         diff = x0 - x1
-        gx0 = gy * diff * (2. / len(diff))
+        gx0 = gy * diff * (2.0 / len(diff))
         gx1 = -gx0
         return gx0, gx1
 
+
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
-    
-    
 
-    
+
+def softmax_cross_entropy_simple(x, t):
+    x, t = as_variable(x), as_variable(t)
+    N = x.shape[0]
+
+    p = softmax_simple(x)
+    p = clip(p, 1e-15, 1.0)  # To avoid log(0)
+    log_p = log(p)
+    tlog_p = log_p[np.arange(N), t.data]
+    y = -1 * sum(tlog_p) / N
+    return y
+
+
+# ========================================================
+# clip
+# ========================================================
+
+
+class Clip(Function):
+    def __init__(self, x_min, x_max):
+        self.x_min = x_min
+        self.x_max = x_max
+
+    def forward(self, x):
+        y = np.clip(x, self.x_min, self.x_max)
+        return y
+
+    def backward(self, gy):
+        (x,) = self.inputs
+        mask = (x.data >= self.x_min) * (x.data <= self.x_max)
+        gx = gy * mask
+        return gx
+
+
+def clip(x, x_min, x_max):
+    return Clip(x_min, x_max)(x)
--- step.py
+++ step.py
@@ -1,8 +1,9 @@
-#44
+# 47
 
 import numpy as np
-import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+
+# import matplotlib.pyplot as plt
+# plt.switch_backend('agg')
 from dezerohit import Variable, Model
 import dezerohit.functions as F
 from dezerohit.utils import plot_dot_graph, sum_to
@@ -10,37 +11,9 @@ import dezerohit.layers as L
 from dezerohit.models import MLP
 from dezerohit import optimizers
 
-np.random.seed(0)
-x = np.random.rand(100, 1)
-y = np.sin(2 * np.pi * x) + np.random.rand(100, 1)
-
-lr = 0.2
-max_iter = 10000
-hidden_size = 10
-
-model = MLP((hidden_size, 1))
-optimizer = optimizers.SGD(lr).setup(model)
-
-for i in range(max_iter):
-    y_pred = model(x)
-    loss = F.mean_squared_error(y, y_pred)
-
-    model.cleargrads()
-    loss.backward()
-
-    optimizer.update()
-    if i % 1000 == 0:
-        print(loss)
-
-plt.scatter(x, y, s=20)
-plt.xlabel('x')
-plt.ylabel('y')
-t = np.arange(0, 1, .01)[:, np.newaxis]
-y_pred = model(t)
-plt.plot(t, y_pred.data, color='r')
-plt.savefig('model_train.png')
-
-
-
-
-
+model = MLP((10, 3))
+x = np.array([[0.2, -0.4], [0.3, 0.5], [1.3, 3.2], [2.1, 0.3]])
+t = np.array([2, 0, 1, 0])
+y = model(x)
+loss = F.softmax_cross_entropy_simple(y, t)
+print(loss)
